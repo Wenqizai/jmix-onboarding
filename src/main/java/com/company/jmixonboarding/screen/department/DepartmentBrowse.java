c5331ba4b8cf77002044659863432180197bd8f1
liangwenqi <wenqizai@gmail.com>
2024-11-27 16:54:29 +0800

    入门-department

---
+++ src/main/java/com/company/jmixonboarding/screen/department/DepartmentBrowse.java
@@ -0,0 +1,13 @@
+package com.company.jmixonboarding.screen.department;
+
+import com.company.jmixonboarding.entity.Department;
+import io.jmix.ui.screen.LookupComponent;
+import io.jmix.ui.screen.StandardLookup;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("Department.browse")
+@UiDescriptor("department-browse.xml")
+@LookupComponent("departmentsTable")
+public class DepartmentBrowse extends StandardLookup<Department> {
+}
---
+++ src/main/java/com/company/jmixonboarding/screen/department/DepartmentEdit.java
@@ -0,0 +1,13 @@
+package com.company.jmixonboarding.screen.department;
+
+import com.company.jmixonboarding.entity.Department;
+import io.jmix.ui.screen.EditedEntityContainer;
+import io.jmix.ui.screen.StandardEditor;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("Department.edit")
+@UiDescriptor("department-edit.xml")
+@EditedEntityContainer("departmentDc")
+public class DepartmentEdit extends StandardEditor<Department> {
+}
