254aabf419eb74e2e2514536882cb389ec0a0b59
liangwenqi <wenqizai@gmail.com>
2024-12-10 16:20:49 +0800

    入门- 数据访问控制

---
+++ src/main/java/com/company/jmixonboarding/security/HRManagerRlRole.java
@@ -0,0 +1,17 @@
+package com.company.jmixonboarding.security;
+
+import com.company.jmixonboarding.entity.Department;
+import com.company.jmixonboarding.entity.User;
+import io.jmix.security.role.annotation.JpqlRowLevelPolicy;
+import io.jmix.security.role.annotation.RowLevelRole;
+
+@RowLevelRole(name = "HR manager’s departments and users", code = HRManagerRlRole.CODE)
+public interface HRManagerRlRole {
+    String CODE = "hr-manager-rl";
+
+    @JpqlRowLevelPolicy(entityClass = Department.class, where = "{E}.hrManager.id = :current_user_id")
+    void department();
+
+    @JpqlRowLevelPolicy(entityClass = User.class, where = "{E}.department.hrManager.id = :current_user_id")
+    void user();
+}
